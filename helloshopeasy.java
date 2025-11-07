0e4514638f08f899ffd406e1a953cf86bb7e8a4b
Sai Gokul Nellore <gokulreddy@Mac.lan>
2025-11-06 18:02:57 -0600

    Initial commit - shopeasy v1.0

---
+++ helloshopeasy.java
@@ -0,0 +1,9 @@
+// Developer s code for ShopEasy app
+public class helloshopeasy {
+    public static void main(String[] args) {
+        System.out.println("🛍️ Welcome to ShopEasy — the easiest way to shop online!");
+        System.out.println("Version 1.0 — Powered by DevOps magic ✨");
+    }
+}
+
+
